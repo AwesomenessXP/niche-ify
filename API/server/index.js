78a327e161f2f0938534e7883496f67ea3c0824a
HaskMoney <heartstring85@gmail.com>
2022-12-20 04:19:05 -0800

    able to generate token and refresh token

--- API/server/index.js
+++ API/server/index.js
@@ -1,13 +1,109 @@
 // set up server
 const express = require('express');
 require('dotenv').config();
-
+const querystring = require('node:querystring');
+const axios = require('axios');
 const PORT = process.env.PORT || 8000;
-
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
   res.json({ client_id: `${process.env.CLIENT_ID}`});
+});
+
+const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+
+// const CLIENT_ID = "084112fb2acf4fa38335818cb7093f58";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const stateKey = 'spotify_auth_state';
+
+app.get('/login', (req, res) => {
+
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = 'user-read-private user-read-email';
+
+  const queryParams = querystring.stringify({
+    client_id: process.env.CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: process.env.REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get('/callback', (req, res) => {
+  const code = req.query.code || null;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: process.env.REDIRECT_URI
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+  .then(response => {
+    if (response.status === 200) {
+      const { access_token, refresh_token } = response.data;
+
+      const queryParams = querystring.stringify({
+        access_token,
+        refresh_token,
+      });
+
+      res.redirect(`http://localhost:3000/?${queryParams}`);
+
+    } else {
+      res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
+    }
+  })
+  .catch(error => {
+    res.send(error);
+  });
+});
+
+app.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
 });
 
 app.listen(PORT, () => {
--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,27 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import SpotifyWebApi from 'spotify-web-api-js';
-
-const spotify = new SpotifyWebApi();
 
 function App() {
-  // initialize states
+  //initialize states
   const [data, setData] = useState('');
-  const [token, setToken] = useState('');
 
   // get client id from server
   useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.client_id)); // rerender
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    console.log(accessToken);
+    console.log(refreshToken);
   }, []);
-
-  // check if we have token saved in local storage
-  useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
-
-    if (!token && hash) {
-      token = hash.substring(1).split("&")
-        .find(elem => elem.startsWith("access_token")).split("=")[1];
-
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
-      spotify.setAccessToken(token);
-    }
-
-    setToken(token);
-    getData();
-
-    async function getData() {
-      const playlists = await spotify.getUserPlaylists();
-      const firstPlaylist = playlists.items[0].id;
-      const tracks = await spotify.getPlaylist(`${firstPlaylist}`, {limit: 100, offset: 1});
-      console.log(tracks);
-    }
-
-  }, []);
-
-  const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-  }
-
-  const searchArtistInput = (event) => {
-    console.log(event);
-  }
-
-  // params to request access token
-  const CLIENT_ID = data;
-  const REDIRECT_URI = 'http://localhost:3000/';
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
-  const scopes = [
-    'playlist-read-private',
-    'playlist-read-collaborative',
-    'playlist-modify-private',
-    'playlist-modify-public',
-  ]
 
   /* if no token, show login, if token, show logout */
-  if (!token) {
-    return (
-      <button>
-        <a href={
-          `${AUTH_ENDPOINT}?` +
-          `client_id=${CLIENT_ID}` +
-          `&redirect_uri=${REDIRECT_URI}` +
-          `&response_type=${RESPONSE_TYPE}` +
-          `&scope=${scopes}`}>
-          Login to Spotify
-        </a>
-      </button>
-    );
-
-  }
-  else {
-    return (
-      <div>
-        <form onSubmit={searchArtistInput}></form>
-        <button onClick={logout}>Log out</button>
-      </div>
-    );
-
-  }
+  return (
+    <a href='http://localhost:8000/login'>
+      Login to Spotify
+    </a>
+  )
 }
 
 export default App;
