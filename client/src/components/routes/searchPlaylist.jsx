a23f5f02ef7610e5db4c89a594346bfec53544ac
HaskMoney <heartstring85@gmail.com>
2022-12-23 02:03:24 -0800

    filtered playlists to ONLY show user's owned playlists

--- client/src/components/routes/searchPlaylist.jsx
+++ client/src/components/routes/searchPlaylist.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react"
-import axios from 'axios';
-import { Routes, Route, Navigate} from 'react-router-dom';
-
+import { useEffect, useState, useRef } from "react"
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { UserPlaylist } from "../playlist/userPlaylists";
 import { ShowOnePlaylist } from '../playlist/showOnePlaylist';
+const Spotify = require('spotify-web-api-js');
 
 /**
  * 
@@ -12,25 +11,41 @@ import { ShowOnePlaylist } from '../playlist/showOnePlaylist';
  * 
  */
 export const GetUserPlaylists = ({ token }) => {
+  // initialize spotify api wrapper
+  const spotifyApi = new Spotify();
+  spotifyApi.setAccessToken(token);
+
   // initialize all states
   const [listOfPlaylists, setListOfPlaylists] = useState([{}]);
   const [playlistTracks, setPlaylistTracks] = useState(null);
   const [playlistName, setPlaylistName] = useState('');
+  const userName = useRef(null);
 
   // happens any time the token is updated/modified
   // fetches all of the user's playlists and stores them
+
   useEffect(() => {
-    axios({
-      method: 'get',
-      baseURL: 'https://api.spotify.com/v1/me/playlists',
-      headers: `Authorization: Bearer ${token}`,
-    })
-      .then(res => {
-        console.log(res.data);
-        setListOfPlaylists(res.data.items);
-      })
-      .catch(err => console.error(err));
-  }, [token]);
+    // get user's name
+    async function getName() {
+      userName.current = await spotifyApi.getMe();
+    }
+
+    // get ONLY user's playlists
+    const playlists = async () => {
+      const fetchPlaylists = await spotifyApi.getUserPlaylists();
+      const modPlaylists = await fetchPlaylists.items.filter((playlist) => {
+        return playlist.owner.display_name === `${userName.current.id}`
+      });
+      setListOfPlaylists(modPlaylists);
+    }
+
+    try {
+      getName().then(playlists);
+    } catch (e) {
+      console.error(e);
+    }
+
+  });
 
   return ( 
     <Routes>
