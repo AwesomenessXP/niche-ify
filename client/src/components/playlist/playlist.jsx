824c3802fcd5e98a161168df9245bf858dfea0a7
HaskMoney <heartstring85@gmail.com>
2023-01-02 00:10:47 -0800

    nicheify function now filters artists by followers

--- client/src/components/playlist/playlist.jsx
+++ client/src/components/playlist/playlist.jsx
@@ -23,17 +23,24 @@ export const ShowOnePlaylist = () => {
     let i = -1;
     const promises = uniqueArtists.map(async (artist) => {
       const artistsRelatedArtists = await spotifyApi.getArtistRelatedArtists(artist);
-      // console.log(artistsRelatedArtists.artists);
       i++;
       return {
         artistName: artistsName[i],
         artistID: artistsID[i],
-        relatedArtists: await artistsRelatedArtists
+        relatedArtists: await artistsRelatedArtists.artists
       };
       
     });
 
-    const relatedArtists = await Promise.all(promises);
+    let relatedArtists = await Promise.all(promises);
+    console.log(relatedArtists.length)
+    // get follow count
+    const followerPromise = relatedArtists.map(async (artist) => {
+      const { artistID } = artist;
+      const artistFollowers = (await spotifyApi.getArtist(artistID)).followers.total;
+      artist.followCount = artistFollowers;
+      return artist;
+    });
     
     // contraints: number of API requests made in 30s
     // for each artistID, check if they HAVE any related artists (greater than 0)
@@ -42,10 +49,10 @@ export const ShowOnePlaylist = () => {
     //             - COMPARE current artist with related artists -> save smallest artist
     // nested loop: compare least popular artist with all related artists - O(n)
     // ----- repeat nested loops until criteria met ----------- O(n)
-
+    // let criteria = 10k followers
     // for (artists in playlist) { O(n)
     //   smallest_artist = current artist
-    //   while (smallest_artist > 10k followers) { O(n)
+    //   while (smallest_artist > criteria) { O(n)
     //     fetch related artists - O(1)
     //     compare current artist with smallest_artist - O(1) (max # of related artists is 20)
     //     save new smallest current artist
@@ -53,18 +60,41 @@ export const ShowOnePlaylist = () => {
     //   return smallest artist;
     // }// for
 
-    relatedArtists.forEach(artist => {
-      const { artistName, artistID, relatedArtists } = artist;
-      console.log(`${artistID}`)
-    })
-
-    // (brute force method): while the number of followers is less than criteria,
+    // (greedy method): while the number of followers is less than criteria,
     //                continue fetching related artist data
     // Summary:
     // call API to get how many followers each artist have
     // find the artist with smallest number of followers
     // check if the artist meets the criteria (lets say <= 10k)
     // if no artists meet criteria, return the original artist (array.length = 0)
+
+    const followers = await Promise.all(followerPromise);
+
+    const replace2Niche = await followers.map((artist) => {
+      const { artistName, artistID, relatedArtists, followCount } = artist;
+      let smallestArtist = {
+        artistName: artistName,
+        artistID: artistID,
+        followCount: followCount
+      }
+      
+      if (relatedArtists.length > 0) {
+        relatedArtists.forEach(relatedArtist => {
+          if (smallestArtist.followCount > relatedArtist.followers.total) {
+            smallestArtist = {
+              artistName: relatedArtist.name,
+              artistID: relatedArtist.id,
+              followCount: relatedArtist.followers.total
+          }}
+        })
+      }
+      return smallestArtist;
+    });
+
+    const nicheArtists = await Promise.all(replace2Niche);
+    nicheArtists.forEach(nicheArtist => {
+      console.log(nicheArtist)
+    })
   }
   /* display all tracks from the playlist */
   return (
