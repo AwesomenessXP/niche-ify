90b2ed940a4f7c70fdf828f6cc8e691be21e3414
HaskMoney <heartstring85@gmail.com>
2023-01-05 05:03:01 -0800

    fixed nicheify algo

--- client/src/components/playlist/playlistCell.jsx
+++ client/src/components/playlist/playlistCell.jsx
@@ -34,55 +34,64 @@ export const PlaylistCell = ({ token }) => {
   async function nicheify() {
     // get list of all artists
     let artistsID = tracks.map(track => track.track.artists[0].id);
+    artistsID = new Set(artistsID);
+    artistsID = Array.from(artistsID);
+
     let artistsName = tracks.map(track => track.track.artists[0].name);
-    // remove dupes
+    artistsName= new Set(artistsName);
+    artistsName = Array.from(artistsName);
+    // // remove dupes
     let uniqueArtists = new Set(artistsID);
-    uniqueArtists = Array.from(uniqueArtists);
+      uniqueArtists = Array.from(uniqueArtists);
 
     //------------------- GET ALL ARTISTS ---------------------------------
     let i = -1;
     const promises = uniqueArtists.map(async (artist) => {
       const relatedArtists = (await spotifyApi
         .getArtistRelatedArtists(artist)).artists;
-      const artistFollowers = (await spotifyApi.getArtist(artist))
-        .followers.total;
+      
+      const artistFollowers = (await spotifyApi.getArtist(artist)).followers.total;
+
       i++;
       // returns an Artist object w/ data to be used in the algorithm
       return new Artist(
         artistsName[i],
         artistsID[i],
-        relatedArtists,
-        artistFollowers,
+        await relatedArtists,
+        await artistFollowers,
       );
       
     });
 
     const followers = await Promise.all(promises);
 
-    // ------------------ NICHE-IFY ARTISTS ------------------------------------
-    const replace2Niche = await followers.map(async (artist) => {
-      let { artistName, artistID, relatedArtists, followCount } = artist;
-      let smallestArtist = new Artist(artistName, artistID, relatedArtists, followCount);
-      console.log('main loop');
-      
-      // while (smallestArtist.followCount > 20000) {
-        console.log('outer loop');
-        // filter through the array and find least popular artist
-        for (let i = 0; i < relatedArtists.length; i++) {
-          console.log('inner loop')
-          const { followers, name, id } = relatedArtists[i];
-          const total = followers.total;
-          // if the related artist is smaller, save that as the smallestArtist
-          if (smallestArtist.followCount > total) {
-            smallestArtist = new Artist(name, id, [], total);
+    // // ------------------ NICHE-IFY ARTISTS ------------------------------------
+    const replace2Niche = followers.map(async (artist) => {
+      let smallestArtist = artist;
+
+        while (smallestArtist.followCount > 20000 && smallestArtist.relatedArtists.length > 0) {
+          console.log('outer loop');
+          console.log(`artist: ${smallestArtist.artistName}`)
+          // filter through the array and find least popular artist
+          for (let i = 0; i < smallestArtist.relatedArtists.length; i++) {
+            console.log('inner loop')
+            const { followers, name, id } = smallestArtist.relatedArtists[i];
+            const total = followers.total;
+            // if the related artist is smaller, save that 
+            if (smallestArtist.followCount > total) {
+              smallestArtist = new Artist(name, id, [], total);
+            }
           }
+          
+          // after finding least popular artist, set their relatedArtists
+          smallestArtist.relatedArtists = (await spotifyApi.getArtistRelatedArtists(smallestArtist.artistID)).artists;
+          
         }
-        
-        // after finding least popular artist, set their relatedArtists
-        relatedArtists = (await spotifyApi.getArtistRelatedArtists(smallestArtist.artistID)).artists;
-      // }
-      
+ 
+    
+      // console.log(`ungrateful artist: ${artistName}, follow count: ${followCount}`)
       return smallestArtist;
+
     });
 
     //Update playlist tracks here:
